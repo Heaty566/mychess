ef52702ce3b87d7bac81649947dff4d3caa0aaa4
tantruong2303 <new9a11516@gmail.com>
2021-02-23 22:05:55 +0700

    add test for saveAuthToken in AuthService

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, Res, UsePipes } from '@nestjs/common';
-import { Response } from "express"
+import { Response } from 'express';
 import { ErrorResponse } from '../app/interface/ErrorResponse';
 import { ApiResponse } from '../app/interface/ApiResponse';
 import { RegisterUserDTO, vRegisterUserDto } from './dto/register.dto';
@@ -10,18 +10,17 @@ import { JoiValidatorPipe } from '../app/validator/validator.pipe';
 import { LoginUserDTO, vLoginUserDto } from './dto/login.dto';
 import { AuthToken } from './entities/authToken.entity';
 import { RefreshToken } from './entities/refreshToken.entity';
-
+import { JwtModule } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-      constructor(private readonly authService: AuthService, private readonly userService: UserService) { }
+      constructor(private readonly authService: AuthService, private readonly userService: UserService) {}
 
       @Post('/register')
       @UsePipes(new JoiValidatorPipe(vRegisterUserDto))
       async registerUser(@Body() body: RegisterUserDTO): Promise<ApiResponse<void>> {
             const user = await this.userService.findOneUserByField('username', body.username);
             if (user) throw ErrorResponse.send({ details: { username: 'Username is already exist.' } }, 'BadRequestException');
-
 
             const newUser = new User();
             newUser.username = body.username;
@@ -35,11 +34,12 @@ export class AuthController {
       @Post('/login')
       @UsePipes(new JoiValidatorPipe(vLoginUserDto))
       async loginUser(@Body() body: LoginUserDTO, @Res() res: Response) {
-            const user = await this.userService.findOneUserByField("username", body.username);
-            if (!user) throw ErrorResponse.send({ details: { username: 'Username or password is not correct.' } }, "BadRequestException");
+            const user = await this.userService.findOneUserByField('username', body.username);
+            if (!user) throw ErrorResponse.send({ details: { username: 'Username or password is not correct.' } }, 'BadRequestException');
 
             // Where hashing???
-            if (user.password !== body.password) throw ErrorResponse.send({ details: { password: 'Username or password is not correct.' } }, "BadRequestException");
+            if (user.password !== body.password)
+                  throw ErrorResponse.send({ details: { password: 'Username or password is not correct.' } }, 'BadRequestException');
 
             // authToken contain:  _id, jwt string(user ==> jwt string)
             let authToken = new AuthToken();
@@ -49,28 +49,24 @@ export class AuthController {
             authToken = await this.authService.saveAuthToken(authToken);
 
             // refreshToken contain _id of authToken, create time, expired time
-            let refreshToken = new RefreshToken();
+            const refreshToken = new RefreshToken();
             refreshToken.data = this.authService.createToken({
-                  authTokenId: authToken._id
-            })
+                  authTokenId: authToken._id,
+            });
 
             // save refreshToken to db
             await this.authService.saveRefreshToken(refreshToken);
-            console.log(refreshToken.data);
-            res.cookie("refresh-token", refreshToken.data).send({});
 
+            return res.cookie('refresh-token', refreshToken.data).send({});
 
             // tao refeshToken gom id, createTime, expiredTime
             // gui refeshToken cho user
-
 
             // const token = sign(
             //       {
             //             userId: user._id
             //       }, process.env.JWT_SECRET_KEY);
 
-
             //return res.cookie("name", token).send();
-
       }
 }
--- server/src/auth/entities/refreshToken.entity.ts
+++ server/src/auth/entities/refreshToken.entity.ts
@@ -1,11 +1,11 @@
-import { ObjectId } from "mongodb";
-import { Column, Entity, ObjectIdColumn } from "typeorm";
+import { ObjectId } from 'mongodb';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
 
 @Entity()
 export class RefreshToken {
-    @ObjectIdColumn()
-    _id: ObjectId;
+      @ObjectIdColumn()
+      _id: ObjectId;
 
-    @Column()
-    data: any;
+      @Column()
+      data: any;
 }
