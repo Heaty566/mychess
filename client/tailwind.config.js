2eaf1b5230c2f35937b0b863eda9873faa25ffb0
heaty566 <heaty566@gmail.com>
2021-02-23 22:45:20 +0700

    done tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+        purge: [],
+        darkMode: false, // or 'media' or 'class'
+        theme: {
+                extend: {
+                        colors: {
+                                "catskill-white": {
+                                        DEFAULT: "#F5F7FA",
+                                        50: "#FFFFFF",
+                                        100: "#FFFFFF",
+                                        200: "#FFFFFF",
+                                        300: "#FFFFFF",
+                                        400: "#FFFFFF",
+                                        500: "#F5F7FA",
+                                        600: "#D3DCE9",
+                                        700: "#B1C1D8",
+                                        800: "#8FA5C7",
+                                        900: "#6D8AB6",
+                                },
+                        },
+                },
+        },
+        variants: {
+                extend: {},
+        },
+        plugins: [],
+};
