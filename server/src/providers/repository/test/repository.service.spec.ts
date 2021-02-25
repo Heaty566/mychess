406e554dd3a1a45206cf4e193c28c0134fba7e29
tantruong2303 <new9a11516@gmail.com>
2021-02-25 12:47:48 +0700

    add test for findManyByArrayValue

--- server/src/providers/repository/test/repository.service.spec.ts
+++ server/src/providers/repository/test/repository.service.spec.ts
@@ -26,18 +26,18 @@ describe('RepositoryService', () => {
                   await userRepository.save(user);
             });
 
-            it('Pass to be defined', async () => {
+            it('Pass (field is not _id)', async () => {
                   const res = await userRepository.findOneByField('name', user.name);
                   expect(res).toBeDefined();
             });
 
-            it('Pass (_id to be defined)', async () => {
+            it('Pass (field is _id)', async () => {
                   const userData = await userRepository.findOneByField('name', user.name);
                   const res = await userRepository.findOneByField('_id', userData._id);
                   expect(res).toBeDefined();
             });
 
-            it('Failed is not valid id', async () => {
+            it('Failed (is not valid _id)', async () => {
                   const res = await userRepository.findOneByField('_id', fakeData(10, 'lettersAndNumbers'));
                   expect(res).toBeNull();
             });
@@ -51,20 +51,55 @@ describe('RepositoryService', () => {
                   await userRepository.save(user);
             });
 
-            it('Pass to be defined', async () => {
+            it('Pass (field is not _id)', async () => {
                   const res = await userRepository.findManyByField('name', user.name);
                   expect(res[0]).toBeDefined();
             });
 
-            it('Pass (_id to be defined)', async () => {
+            it('Pass (field is _id)', async () => {
                   const userData = await userRepository.findOneByField('name', user.name);
                   const res = await userRepository.findManyByField('_id', userData._id);
                   expect(res[0]).toBeDefined();
             });
 
-            it('Failed is not valid id', async () => {
+            it('Failed (is not valid _id)', async () => {
                   const res = await userRepository.findManyByField('_id', fakeData(10, 'lettersAndNumbers'));
                   expect(res).toBeNull();
+            });
+      });
+
+      describe('findManyByArrayValue', () => {
+            let user1: User;
+            let user2: User;
+            let value = [];
+
+            beforeEach(async () => {
+                  user1 = fakeUser();
+                  user2 = fakeUser();
+                  await userRepository.save(user1);
+                  await userRepository.save(user2);
+            });
+
+            it('Pass (field is not _id)', async () => {
+                  value = [user1.name, user2.name];
+
+                  const res = await userRepository.findManyByArrayValue('name', value, null);
+                  expect(res[value.length - 1]).toBeDefined();
+            });
+
+            it('Pass (field is _id)', async () => {
+                  const userData1 = await userRepository.findOneByField('name', user1.name);
+                  const userData2 = await userRepository.findOneByField('name', user2.name);
+
+                  value = [userData1._id, userData2._id];
+
+                  const res = await userRepository.findManyByArrayValue('_id', value, null);
+                  expect(res[value.length - 1]).toBeDefined();
+            });
+
+            it('Failed (value is [])', async () => {
+                  const res = await userRepository.findManyByArrayValue('name', value, null);
+                  expect(res[0]).toBeUndefined();
             });
       });
 
