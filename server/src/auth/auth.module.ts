6d159cfd96f73aaf93a5d63e4fd5b2d3c933ae0c
heaty566 <heaty566@gmail.com>
2021-02-26 01:43:32 +0700

    fix circle injection

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,18 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from '../user/entities/user.repository';
 import { UserModule } from '../user/user.module';
-import { UserService } from '../user/user.service';
 import { AuthTokenRepository } from './entities/authToken.repository';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
-      imports: [TypeOrmModule.forFeature([AuthTokenRepository]), forwardRef(() => UserModule)],
+      imports: [TypeOrmModule.forFeature([AuthTokenRepository]), UserModule],
       controllers: [AuthController],
       providers: [
             AuthService,
-            UserService,
             {
                   provide: JwtService,
                   useFactory: () => {
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-      constructor(private readonly userService: UserService) {}
+      constructor(private readonly userService: UserService, private readonly authService: AuthService) {}
 }
