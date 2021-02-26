8bbdc254f8afc7dba6efddfcded13cdafc281f7c
heaty566 <heaty566@gmail.com>
2021-02-26 14:24:42 +0700

    inject localesService

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
