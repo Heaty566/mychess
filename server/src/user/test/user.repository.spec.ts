50aef86bc9ba20aec7fc1bbb9a667db0e5b8582f
unknown <new9a11516@gmail.com>
2021-03-04 00:20:39 +0700

    add test for user repository

---
+++ server/src/user/test/user.repository.spec.ts
@@ -0,0 +1,42 @@
+//* Internal import
+import { INestApplication } from '@nestjs/common';
+import { UserRepository } from '../../user/entities/user.repository';
+import { initTestModule } from '../../../test/initTest';
+import { User } from '../../user/entities/user.entity';
+import { fakeData } from '../../../test/fakeData';
+
+describe('UserRepository', () => {
+      let app: INestApplication;
+      let userRepository: UserRepository;
+      let userDb: User;
+
+      beforeAll(async () => {
+            const { getUser, getApp, module } = await initTestModule();
+            app = getApp;
+            userDb = getUser;
+            userRepository = module.get<UserRepository>(UserRepository);
+      });
+
+      describe('getUserByField', () => {
+            it('Pass (field is not _id)', async () => {
+                  const res = await userRepository.findOneByField('name', userDb.name);
+                  expect(res).toBeDefined();
+            });
+
+            it('Pass (field is _id)', async () => {
+                  const userData = await userRepository.findOneByField('name', userDb.name);
+                  const res = await userRepository.findOneByField('_id', userData._id.toHexString());
+                  expect(res).toBeDefined();
+            });
+
+            it('Failed (is not valid _id)', async () => {
+                  const res = await userRepository.findOneByField('_id', fakeData(10, 'lettersAndNumbers'));
+                  expect(res).toBeNull();
+            });
+      });
+
+      afterAll(async () => {
+            await userRepository.clear();
+            await app.close();
+      });
+});
