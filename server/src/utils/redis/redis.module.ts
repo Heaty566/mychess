3cfb9f5b161f08aea92c6f81b8d6cf210f0e9cea
heaty566 <heaty566@gmail.com>
2021-03-05 19:34:53 +0700

    more test

--- server/src/utils/redis/redis.module.ts
+++ server/src/utils/redis/redis.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { createClient } from 'redis';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
+      imports: [LoggerModule],
       controllers: [],
       providers: [
             RedisService,
