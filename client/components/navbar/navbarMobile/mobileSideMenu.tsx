a15497b76f1edf893f3d1884ae7d37df69e186e4
heaty566 <heaty566@gmail.com>
2021-04-12 01:44:54 +0700

    navbar user

--- client/components/navbar/navbarMobile/mobileSideMenu.tsx
+++ client/components/navbar/navbarMobile/mobileSideMenu.tsx
@@ -1,29 +1,37 @@
 import * as React from 'react';
 import Link from 'next/link';
 
-import NavbarLang, { THandleChangeLanguage } from '../navbarLang';
+import routers from '../../../common/constants/router';
+
+import navbarMenuConfig from '../navbarMenu/config';
+import ArrowDropDownMenu from '../../arrowDropDownMenu';
 import config from './config';
+import NavbarLang from '../navbarLang';
+import { IAuthState } from '../../../store/auth/interface';
 
 export interface MobileNavSideMenuProps {
     isActive: boolean;
-    isActiveLang: boolean;
-    handleChangeActiveLang(): void;
-    handleChangeLanguage: THandleChangeLanguage;
+    authState: IAuthState;
+    handleChangeLanguage: (data: any) => void;
+    handleLogoutUser: () => void;
 }
 
-const MobileNavSideMenu: React.FunctionComponent<MobileNavSideMenuProps> = ({
-    isActive,
-    isActiveLang,
-    handleChangeActiveLang,
-    handleChangeLanguage,
-}) => (
+const MobileNavSideMenu: React.FunctionComponent<MobileNavSideMenuProps> = ({ isActive, handleChangeLanguage, authState, handleLogoutUser }) => (
     <div
         className={`md:hidden fixed w-64 bg-woodsmoke top-0 left-0 h-screen z-40 transform duration-300 shadow-menu ${
             isActive ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'
         }`}
     >
         <ul className="text-cloud">
-            {config.map((item) => (
+            <li>
+                <Link href={`${routers.userProfile.link}/${authState.id}`}>
+                    <div className="flex items-center p-4 space-x-2 capitalize duration-300 cursor-pointer hover:text-white">
+                        <img className="object-cover w-8 h-8" src={authState.avatarUrl} alt={authState.name} />
+                        <a href={`${routers.userProfile.link}/${authState.id}`}>{authState.name}</a>
+                    </div>
+                </Link>
+            </li>
+            {navbarMenuConfig.map((item) => (
                 <li key={item.label} className="duration-300 hover:bg-woodsmoke-400">
                     <Link href={item.link}>
                         <a href={item.link} className="block p-4 ">
@@ -32,13 +40,26 @@ const MobileNavSideMenu: React.FunctionComponent<MobileNavSideMenuProps> = ({
                     </Link>
                 </li>
             ))}
+            {config.map(
+                (item) =>
+                    item.isAppearOnLogin === authState.isLogin && (
+                        <li key={item.label} className="duration-300 hover:bg-woodsmoke-400">
+                            <Link href={item.link}>
+                                <a href={item.link} className="block p-4 ">
+                                    {item.label}
+                                </a>
+                            </Link>
+                        </li>
+                    ),
+            )}
+
             <li className="p-4 uppercase duration-300 hover:bg-woodsmoke-400">
-                <NavbarLang
-                    isActive={isActiveLang}
-                    handleChangeActive={handleChangeActiveLang}
-                    handleChangeLanguage={handleChangeLanguage}
-                    dropMenuPosition="left-0"
-                />
+                <ArrowDropDownMenu Component={<NavbarLang handleOnChangeLanguage={handleChangeLanguage} />} dropMenuPosition="left-0">
+                    <span className="mr-1 duration-300 hover:text-cloud-50">Language</span>
+                </ArrowDropDownMenu>
+            </li>
+            <li className="p-4 uppercase duration-300 cursor-pointer hover:bg-woodsmoke-400">
+                <button onClick={() => handleLogoutUser()}>LOGOUT</button>
             </li>
         </ul>
     </div>
