bef8553c829471d06993d034b55fa07d531fafcf
tantruong2303 <new9a11516@gmail.com>
2021-04-27 13:18:03 +0700

    add notification action

---
+++ server/src/notifications/notifications.action.ts
@@ -0,0 +1,6 @@
+export enum NotificationAction {
+      NOTI_CONNECTION = 'connection-notification',
+      NOTI_SEND = 'send-notification',
+      NOTI_REFRESH = 'refresh-notification',
+      NOTI_NEW = 'new-notification',
+}
