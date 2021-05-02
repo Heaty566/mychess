148ca4afbcc5580b3a7c8a7ee01dbaebd8579008
tantruong2303 <new9a11516@gmail.com>
2021-05-02 15:45:37 +0700

    fix test for notification

--- server/src/notifications/entities/notificationAddFriendType.entity.ts
+++ server/src/notifications/entities/notificationAddFriendType.entity.ts
@@ -3,22 +3,13 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 //---- Entity
 import User from '../../users/entities/user.entity';
 
-@Entity()
-export class NotificationAddFriendType {
-      constructor(content: string, sender: string) {
-            this.content = content;
-            this.sender = sender;
-      }
+// @Entity()
+// export class NotificationAddFriendType {
+//       constructor(content: string, sender: string) {
+//             this.content = content;
+//             this.sender = sender;
+//       }
 
-      @PrimaryGeneratedColumn('uuid')
-      id: string;
-
-      @Column({ default: new Date().toISOString().slice(0, 19).replace('T', ' ') })
-      createDate: Date;
-
-      @Column({ default: null })
-      content: string;
-
-      @Column({ default: null })
-      sender: string;
-}
+//       @PrimaryGeneratedColumn('uuid')
+//       id: string;
+// }
