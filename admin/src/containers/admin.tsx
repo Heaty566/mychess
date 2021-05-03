70ff0054b8a0af78805650b58e4693679ebe2d25
kainesv86 <kainesv86@gmail.com>
2021-05-03 16:03:14 +0700

    admin page

--- admin/src/containers/admin.tsx
+++ admin/src/containers/admin.tsx
@@ -1,38 +1,40 @@
 import * as React from 'react';
 
-import { List, Row, Col, Avatar } from 'antd';
+import { List, Row, Col, Avatar, Skeleton } from 'antd';
 import { http } from '../service/http';
 export interface AdminProps {}
 
+interface UserProp {
+    name: string;
+    username: string;
+    avatarUrl: string;
+    email: string;
+    role: 'USER' | 'ADMIN';
+}
+
 const Admin: React.FunctionComponent<AdminProps> = () => {
-    const [users, setUsers] = React.useState();
+    const [users, setUsers] = React.useState<Array<UserProp>>([]);
 
     React.useEffect(() => {
         (async () => {
-            const users = await http.get('/admin/users');
-            console.log(users);
+            const data = await http.get('/admin/users');
+            setUsers(data.data.data);
         })();
-    }, []);
+    }, [setUsers]);
 
     return (
-        <Row justify="start" align="top" className="h-full" style={{ margin: '64px' }}>
-            <Col>
-                <List.Item key={1}>
-                    <List.Item.Meta
-                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                        title={<a href="https://ant.design">Kaine</a>}
-                        description={'kainesv@gmail.com'}
-                    />
-                    <div>Content</div>
-                </List.Item>
-                <List.Item key={2}>
-                    <List.Item.Meta
-                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                        title={<a href="https://ant.design">Kaine</a>}
-                        description={'kainesv@gmail.com'}
-                    />
-                    <div>Content</div>
-                </List.Item>
+        <Row justify="start" align="top" className="h-full m-48">
+            <Col className="w-full">
+                {users.map((item) => {
+                    return (
+                        <List.Item actions={[<a key="list-loadmore-edit">Edit</a>]}>
+                            <Skeleton avatar title={false} loading={false} active>
+                                <List.Item.Meta avatar={<Avatar src={item.avatarUrl} />} title={`${item.name}`} description={`${item.username}`} />
+                                <div>{`Role: ${item.role}`}</div>
+                            </Skeleton>
+                        </List.Item>
+                    );
+                })}
             </Col>
         </Row>
     );
