8b84c89fd38fdf4cd7536cc26dc5a9c4f23d449d
Cao Chi Hai <haicao2805@gmail.com>
2021-05-05 22:47:02 +0700

    R, B, Q, N, K

--- server/src/chess/test/chess.service.spec.ts
+++ server/src/chess/test/chess.service.spec.ts
@@ -32,28 +32,261 @@ describe('ticTacToeService', () => {
             chessService = module.get<ChessService>(ChessService);
             redisService = module.get<RedisService>(RedisService);
       });
-
-      describe('kingVailableMove', () => {
+      /*
+      describe('kingAvailableMove', () => {
             let chessBoard: ChessBoard;
             beforeEach(() => {
                   chessBoard = new ChessBoard();
             });
 
             it('x = 1, y = 1', async () => {
-                  const result = chessService.kingVailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KING }, chessBoard);
+                  const result = chessService.kingAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KING }, chessBoard);
                   expect(result.length).toBe(8);
             });
             it('x = 1, y = 1', async () => {
-                  chessBoard.board[0][0].chess = ChessRole.PAWN;
-                  chessBoard.board[0][0].flag = 0;
+                  chessBoard.board[0][0] = {
+                        chessRole: ChessRole.PAWN,
+                        flag: 0,
+                  };
 
-                  const result = chessService.kingVailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KING }, chessBoard);
-
-                  console.log(result);
+                  const result = chessService.kingAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KING }, chessBoard);
                   expect(result.length).toBe(7);
+            });
+
+            it('x = 0, y = 0', async () => {
+                  const result = chessService.kingAvailableMove({ flag: 0, x: 0, y: 0, chessRole: ChessRole.KING }, chessBoard);
+
+                  expect(result.length).toBe(3);
+            });
+
+            it('x = 1, y = 1', async () => {
+                  chessBoard.board[0][0] = {
+                        chessRole: ChessRole.PAWN,
+                        flag: 1,
+                  };
+
+                  const result = chessService.kingAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KING }, chessBoard);
+
+                  expect(result.length).toBe(8);
             });
       });
 
+      describe('knightAvailableMove', () => {
+            let chessBoard: ChessBoard;
+            beforeEach(() => {
+                  chessBoard = new ChessBoard();
+            });
+
+            it('x = 1, y = 1', () => {
+                  const result = chessService.knightAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.KNIGHT }, chessBoard);
+                  expect(result.length).toBe(4);
+            });
+
+            it('x = 7, y = 7', () => {
+                  const result = chessService.knightAvailableMove({ flag: 0, x: 7, y: 7, chessRole: ChessRole.KNIGHT }, chessBoard);
+                  expect(result.length).toBe(2);
+            });
+
+            it('x = 4, y = 4', () => {
+                  const result = chessService.knightAvailableMove({ flag: 0, x: 4, y: 4, chessRole: ChessRole.KNIGHT }, chessBoard);
+                  expect(result.length).toBe(8);
+            });
+
+            it('x = 7, y = 7', () => {
+                  chessBoard.board[5][6] = {
+                        chessRole: ChessRole.BISHOP,
+                        flag: 1,
+                  };
+
+                  chessBoard.board[6][5] = {
+                        chessRole: ChessRole.BISHOP,
+                        flag: 1,
+                  };
+
+                  const result = chessService.knightAvailableMove({ flag: 0, x: 7, y: 7, chessRole: ChessRole.KNIGHT }, chessBoard);
+                  expect(result.length).toBe(2);
+            });
+
+            it('x = 7, y = 7', () => {
+                  chessBoard.board[5][6] = {
+                        chessRole: ChessRole.BISHOP,
+                        flag: 1,
+                  };
+
+                  chessBoard.board[6][5] = {
+                        chessRole: ChessRole.BISHOP,
+                        flag: 0,
+                  };
+
+                  const result = chessService.knightAvailableMove({ flag: 0, x: 7, y: 7, chessRole: ChessRole.KNIGHT }, chessBoard);
+                  expect(result.length).toBe(1);
+            });
+      });
+
+      describe('rookAvailableMove', () => {
+            let chessBoard: ChessBoard;
+            beforeEach(() => {
+                  chessBoard = new ChessBoard();
+            });
+
+            it('x = 1, y = 1', () => {
+                  const result = chessService.rookAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.ROOK }, chessBoard);
+                  expect(result.length).toBe(14);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[1][5] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+
+                  const result = chessService.rookAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.ROOK }, chessBoard);
+                  expect(result.length).toBe(11);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[1][0] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[0][1] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[1][2] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[2][1] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  const result = chessService.rookAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.ROOK }, chessBoard);
+                  expect(result.length).toBe(0);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[1][0] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[0][1] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[1][2] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[2][1] = {
+                        flag: 0,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  const result = chessService.rookAvailableMove({ flag: 1, x: 1, y: 1, chessRole: ChessRole.ROOK }, chessBoard);
+                  expect(result.length).toBe(4);
+            });
+      });
+
+      describe('bishopAvailableMove', () => {
+            let chessBoard: ChessBoard;
+            beforeEach(() => {
+                  chessBoard = new ChessBoard();
+            });
+
+            it('x = 1, y = 1', () => {
+                  const result = chessService.bishopAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.BISHOP }, chessBoard);
+                  expect(result.length).toBe(9);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[0][0] = {
+                        flag: 1,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[2][2] = {
+                        flag: 1,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+
+                  const result = chessService.bishopAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.BISHOP }, chessBoard);
+                  expect(result.length).toBe(4);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[0][0] = {
+                        flag: 1,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+                  chessBoard.board[2][2] = {
+                        flag: 1,
+                        chessRole: ChessRole.KNIGHT,
+                  };
+
+                  const result = chessService.bishopAvailableMove({ flag: 1, x: 1, y: 1, chessRole: ChessRole.BISHOP }, chessBoard);
+                  expect(result.length).toBe(2);
+            });
+
+            it('x = 4, y = 4', () => {
+                  const result = chessService.bishopAvailableMove({ flag: 0, x: 4, y: 4, chessRole: ChessRole.BISHOP }, chessBoard);
+                  expect(result.length).toBe(13);
+            });
+      });
+*/
+
+      describe('queenAvailable', () => {
+            let chessBoard: ChessBoard;
+            beforeEach(() => {
+                  chessBoard = new ChessBoard();
+            });
+
+            it('x = 1, y = 1', () => {
+                  const result = chessService.queenAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.QUEEN }, chessBoard);
+                  expect(result.length).toBe(23);
+            });
+
+            it('x = 4, y = 4', () => {
+                  const result = chessService.queenAvailableMove({ flag: 0, x: 4, y: 4, chessRole: ChessRole.QUEEN }, chessBoard);
+                  expect(result.length).toBe(27);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[0][0] = {
+                        flag: 1,
+                        chessRole: ChessRole.BISHOP,
+                  };
+
+                  chessBoard.board[0][1] = {
+                        flag: 1,
+                        chessRole: ChessRole.BISHOP,
+                  };
+
+                  chessBoard.board[2][2] = {
+                        flag: 1,
+                        chessRole: ChessRole.BISHOP,
+                  };
+                  const result = chessService.queenAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.QUEEN }, chessBoard);
+                  expect(result.length).toBe(18);
+            });
+
+            it('x = 1, y = 1', () => {
+                  chessBoard.board[0][0] = {
+                        flag: 1,
+                        chessRole: ChessRole.BISHOP,
+                  };
+
+                  chessBoard.board[2][1] = {
+                        flag: 0,
+                        chessRole: ChessRole.BISHOP,
+                  };
+
+                  chessBoard.board[2][2] = {
+                        flag: 0,
+                        chessRole: ChessRole.BISHOP,
+                  };
+                  const result = chessService.queenAvailableMove({ flag: 0, x: 1, y: 1, chessRole: ChessRole.QUEEN }, chessBoard);
+                  expect(result.length).toBe(11);
+            });
+      });
       afterAll(async () => {
             await resetDB();
             await app.close();
